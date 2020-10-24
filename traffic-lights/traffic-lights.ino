721e3f00322b554af2891ef0b301130a2cfeb04b
Ivan Ilves <ivan.ilves@typeform.com>
2020-10-24 17:46:31 +0200

    More decent state machine!

--- traffic-lights/traffic-lights.ino
+++ traffic-lights/traffic-lights.ino
@@ -1,47 +1,123 @@
-#define RED_PIN 2
-#define YELLOW_PIN 3
-#define GREEN_PIN 4
+#define PIN_RED 2
+#define PIN_YELLOW 3
+#define PIN_GREEN 4
 
-#define NORMAL_DELAY 5000
-#define SHORT_DELAY 2000
+#define COLOR_RED 0
+#define COLOR_YELLOW 1
+#define COLOR_GREEN 2
+
+#define DELAY_RED 5000
+#define DELAY_YELLOW 1500
+#define DELAY_GREEN 5000
 
 #define MODE_AUTO 0
 
-byte mode = MODE_AUTO;
+unsigned long lastSwitchTime = millis();
+
+unsigned int mode = MODE_AUTO;
+unsigned int color = COLOR_RED;
+unsigned int prevColor;
+bool on = true;
 
 void setup() {
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(YELLOW_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
 }
 
-void redLightOn() {
-  digitalWrite(RED_PIN, HIGH);
-  digitalWrite(YELLOW_PIN, LOW);
-  digitalWrite(GREEN_PIN, LOW);
+void setRedLight(bool on) {
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_GREEN, LOW);
+
+  if (on) {
+    digitalWrite(PIN_RED, HIGH);
+  } else {
+    digitalWrite(PIN_RED, LOW);
+  }
 }
 
-void yellowLightOn() {
-  digitalWrite(RED_PIN, LOW);
-  digitalWrite(YELLOW_PIN, HIGH);
-  digitalWrite(GREEN_PIN, LOW);
+void setYellowLight(bool on) {
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_GREEN, LOW);
+
+  if (on) {
+    digitalWrite(PIN_YELLOW, HIGH);
+  } else {
+    digitalWrite(PIN_YELLOW, LOW);
+  }
 }
 
-void greenLightOn() {
-  digitalWrite(RED_PIN, LOW);
-  digitalWrite(YELLOW_PIN, LOW);
-  digitalWrite(GREEN_PIN, HIGH);
+void setGreenLight(bool on) {
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_YELLOW, LOW);
+
+  if (on) {
+    digitalWrite(PIN_GREEN, HIGH);
+  } else {
+    digitalWrite(PIN_GREEN, LOW);
+  }
+}
+
+void setAllLightsOn() {
+  digitalWrite(PIN_RED, HIGH);
+  digitalWrite(PIN_YELLOW, HIGH);
+  digitalWrite(PIN_GREEN, HIGH);
+}
+
+void setLight(int color, bool on) {
+  switch (color) {
+    case COLOR_RED:
+      setRedLight(on);
+      break;
+    case COLOR_YELLOW:
+      setYellowLight(on);
+      break;
+    case COLOR_GREEN:
+      setGreenLight(on);
+      break;
+    default:
+      setAllLightsOn();
+      break;
+  }
 }
 
 void loop() {
-  if (mode == MODE_AUTO) {
-    redLightOn();
-    delay(NORMAL_DELAY);
-    yellowLightOn();
-    delay(SHORT_DELAY);
-    greenLightOn();
-    delay(NORMAL_DELAY);
-    yellowLightOn();
-    delay(SHORT_DELAY);
+  switch (mode) {
+    case MODE_AUTO:
+      switch (color) {
+        case COLOR_RED:
+          if (millis() - lastSwitchTime > DELAY_RED) {
+            lastSwitchTime = millis();
+            color = COLOR_YELLOW;
+            prevColor = COLOR_RED;
+          }
+          break;
+        case COLOR_YELLOW:
+          if (millis() - lastSwitchTime > DELAY_YELLOW) {
+            lastSwitchTime = millis();
+            if (prevColor == COLOR_GREEN) {
+              color = COLOR_RED;
+            } else {
+              color = COLOR_GREEN;
+            }
+            prevColor = COLOR_YELLOW;
+          }
+          break;
+        case COLOR_GREEN:
+          if (millis() - lastSwitchTime > DELAY_GREEN) {
+            lastSwitchTime = millis();
+            color = COLOR_YELLOW;
+            prevColor = COLOR_GREEN;
+          }
+          break;
+        default:
+          setAllLightsOn();
+          break;
+      }
+      break;
   }
+
+  setLight(color, on);
+
+  delay(10);
 }
