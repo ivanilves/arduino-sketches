81844188f066e8238c88a3a95191fcd1dc74bf15
Ivan Ilves <ivan.ilves@typeform.com>
2021-12-18 12:05:27 +0100

    feat(hexalamp): polish the algorithm

--- hexalamp/hexalamp.ino
+++ hexalamp/hexalamp.ino
@@ -1,24 +1,45 @@
-#define ONOFF_PIN 2
-#define IRSIG_PIN A0
-#define ONOFF_TRH 800
+#define DEBUG
+#define ONOFF_PIN 3
+#define IRSIG_PIN A6
+#define ONOFF_TRH 900
 
 void setup() {
   pinMode(ONOFF_PIN, OUTPUT);
   pinMode(IRSIG_PIN, INPUT);
+
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
 }
 
+bool on = false;
 int av;
+unsigned long t = 0;
 
 void loop() {
   av = analogRead(IRSIG_PIN);
 
+#ifdef DEBUG
+  Serial.println(av);
+#endif
 
-  if (av <= ONOFF_TRH) {
-    digitalWrite(ONOFF_PIN, HIGH);
-  } else {
-    digitalWrite(ONOFF_PIN, LOW);
+  if (av < ONOFF_TRH) {
+    on = !on;
+
+    digitalWrite(ONOFF_PIN, on);
+    delay(500);
+
+    if (on) {
+      t = millis();
+    }
   }
 
+  if (on) {
+    if (millis() - t > 30000) {
+      digitalWrite(ONOFF_PIN, !on);
+      t = millis();
+    }
+  }
 
-  delay(100);
+  delay(50);
 }
